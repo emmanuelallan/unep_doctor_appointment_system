de900d9f1c998c684ab65f3914a576a8b3ae0498
maxthestranger <thestrangermax@gmail.com>
2023-01-06 02:07:28 +0300

    add middlewares

--- routes/web.php
+++ routes/web.php
@@ -26,8 +26,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('doctors', \App\Http\Controllers\DoctorsController::class)->middleware('is_admin');
 });
 
 require __DIR__.'/auth.php';
 
-Route::resource('doctors', \App\Http\Controllers\DoctorsController::class);
