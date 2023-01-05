0c35979f64e7c4774cf19e36155745111e94a973
maxthestranger <thestrangermax@gmail.com>
2023-01-05 21:20:34 +0300

    add doctors nav link

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('doctors.index')" :active="request()->routeIs('doctors')">
+                        {{ __('Doctors') }}
+                    </x-nav-link>
                 </div>
             </div>
 
